e75f2e56c12abe27dd46811aeb71503f849f0a01
Roman Parajuli <notorious.garage@gmail.com>
2021-05-02 18:26:50 +0545

    Amazon clone with firebase integration, home page, add / remove to cart functionalities

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCzEI5UcqIb-UcA1lhsFA0MDaCi_szRb8s",
+  authDomain: "clone-3e9a2.firebaseapp.com",
+  projectId: "clone-3e9a2",
+  storageBucket: "clone-3e9a2.appspot.com",
+  messagingSenderId: "220311514249",
+  appId: "1:220311514249:web:986ca4a3b375dbbe97aa58",
+  measurementId: "G-6KS56PVV4R",
+};
